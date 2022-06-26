44a4135c352fcd046dbf92073b698b9de736bee4
B11056051 <B11056051@mail.npust.edu.tw>
2022-06-27 00:25:11 +0800

    Upload at 2022 06 20 (Modified at 2022 06 27)

--- Final/Scripts/Random_Number.js
+++ Final/Scripts/Random_Number.js
@@ -2,19 +2,24 @@ var Min = document.getElementById("Min");
 var Max = document.getElementById("Max");
 var Num = document.getElementById("Num"); //要抽出多少個數字
 var Count = document.getElementById("Count"); // 重複抽的次數
-var btn = document.getElementById("btn");
+var btn_run = document.getElementById("btn-run");
+var fa_solid = document.getElementsByClassName("fa-solid")[1];
+var btn_stop = document.getElementById("btn-stop");
+//var btn_play = document.getElementsByClassName("fa-play")[0];
+var btn_clear = document.getElementById("btn-clear");
 var result = document.getElementById("result");
 var Sum_Count = document.getElementById("Sum_Counts"); //顯示執行次數
 var Speed = document.getElementById("Speed"); //執行速度
 
-btn.addEventListener("click", function() {
+//隱藏暫停按鈕
+btn_stop.style.display = "none";
+
+btn_run.addEventListener("click", function() {
     var min = Number(Min.value);
     var max = Number(Max.value);
     var num = Number(Num.value);
     var count = Number(Count.value);
     var speed = Number(Speed.value);
-
-
 
     //判斷錯誤
     if (min == "" || max == "" || num == "") {
@@ -25,9 +30,19 @@ btn.addEventListener("click", function() {
         Toast("最小值不可大於最大值!");
     } else if (num > max - min + 1) {
         Toast("抽出的數量已超出範圍!");
-    } else if (count < 0) {
+    }
+    /*
+    else if (num >= 1000) {
+        Toast("指定抽出的數量較大，可能會造成網頁當機，是否繼續執行?");
+    } 
+    */
+    else if (count < 0) {
         Toast("執行次數請大於0!");
     } else {
+
+        //顯示暫停按鈕
+        //btn_stop.style.display = "";
+
         if (count == 0) {
             count = 1;
         }
@@ -37,7 +52,7 @@ btn.addEventListener("click", function() {
         var speed = 0.1; //執行速度(ms)
 
         while (z < count) {
-            setTimeout(() => {
+            function Run() {
                 result.innerHTML = "";
                 Sum_Count.innerHTML = "";
                 var array = [];
@@ -64,12 +79,8 @@ btn.addEventListener("click", function() {
                     }
                 }
 
-                //輸出
+                //輸出結果
                 for (var t = 0; t < array.length; t++) {
-                    //如果太多數字，就換行
-                    if (t % 20 == 0) {
-                        result.innerHTML += "<br>";
-                    }
                     result.innerHTML += array[t] + ", ";
                     //去掉最後一個逗號
                     if (t == array.length - 1) {
@@ -81,11 +92,36 @@ btn.addEventListener("click", function() {
                 Steps++;
                 Sum_Count.innerHTML = `<p> (已執行${Steps}次)</p>`;
 
-            }, (speed * 1000) * z)
+                /*
+                //偵測暫停按鈕
+                btn_stop.addEventListener("click", function() {
+                    clearTimeout(Run);
+
+                    //切換暫停按鈕為播放按鈕
+                    fa_solid.classList.remove('fa-stop');
+                    fa_solid.classList.add('fa-play');
+                });
+
+                //偵測播放按鈕
+                btn_play.addEventListener("click", function() {
+                    fa_solid.classList.remove('fa-play');
+                    fa_solid.classList.add('fa-stop');
+                });
+                */
+            }
+
+            setTimeout(Run, (speed * 1000) * z);
 
             z++;
         }
+
     }
+});
+
+
+btn_clear.addEventListener("click", function() {
+    result.innerHTML = "";
+    Sum_Count.innerHTML = "";
 });
 
 //彈出提示框
