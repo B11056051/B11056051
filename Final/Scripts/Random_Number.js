c640a3dcd329da2504659551055a0984f701b64b
B11056051 <B11056051@mail.npust.edu.tw>
2022-06-26 16:28:10 +0800

    Upload at 2022 06 20 (Modified at 2022 06 26)

--- Final/Scripts/Random_Number.js
+++ Final/Scripts/Random_Number.js
@@ -1,31 +1,49 @@
-var min = document.getElementById("min");
-var max = document.getElementById("max");
-var num = document.getElementById("num"); //要抽出多少個數字
+var Min = document.getElementById("Min");
+var Max = document.getElementById("Max");
+var Num = document.getElementById("Num"); //要抽出多少個數字
 var btn = document.getElementById("btn");
 var result = document.getElementById("result");
 
 
 btn.addEventListener("click", function() {
-    //清空result
+    var min = Number(Min.value);
+    var max = Number(Max.value);
+    var num = Number(Num.value);
+
+    //清空結果區域
     result.innerHTML = "";
-    if (min.value == "" || max.value == "" || num.value == "") {
-        alert("請輸入完整資料!");
-    } else if (num.value <= 0) {
-        alert("抽出的數量請大於0!");
-    } else if (min.value > max.value) {
-        alert("最小值不可大於最大值!");
-    } else if (num.value > max.value - min.value + 1) {
-        alert("抽出的數量已超出範圍!");
+    if (min == "" || max == "" || num == "") {
+        Toast("請輸入完整資料!");
+    } else if (num <= 0) {
+        Toast("抽出的數量請大於0!");
+    } else if (min > max) {
+        Toast("最小值不可大於最大值!");
+    } else if (num > max - min + 1) {
+        Toast("抽出的數量已超出範圍!");
     } else {
         var array = [];
-        for (var i = 0; i < num.value; i++) {
-            var random = Math.floor(Math.random() * (parseInt(max.value) - parseInt(min.value) + 1) + parseInt(min.value));
+        for (var i = 0; i < num; i++) {
+            var random = Math.floor(Math.random() * (max - min + 1) + min);
+            //不重複數字判斷
             if (array.indexOf(random) == -1) {
                 array.push(random);
             } else {
                 i--;
             }
         }
+
+        //泡沫排序法
+        for (var k = 0; k < array.length; k++) {
+            for (var o = 0; o < array.length; o++) {
+                if (array[o] > array[o + 1]) {
+                    let temp = 0;
+                    temp = array[o];
+                    array[o] = array[o + 1];
+                    array[o + 1] = temp;
+                }
+            }
+        }
+
         //如果太多數字，就換行
         for (var i = 0; i < array.length; i++) {
             if (i % 20 == 0) {
@@ -39,3 +57,13 @@ btn.addEventListener("click", function() {
         }
     }
 });
+
+//彈出提示框
+var toastLiveExample = document.getElementById('liveToast'); // 提示框
+var host_name = document.getElementById('host_name-B'); //寫入訊息
+
+function Toast(message) {
+    host_name.innerHTML = message;
+    var toast = new bootstrap.Toast(toastLiveExample);
+    toast.show()
+}
