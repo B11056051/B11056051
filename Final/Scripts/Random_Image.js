47fad22f3a2196cbe25260fc471522b449563350
B11056051 <B11056051@mail.npust.edu.tw>
2022-06-29 00:27:36 +0800

    Upload at 2022 06 20 (Modified at 2022 06 29)

--- Final/Scripts/Random_Image.js
+++ Final/Scripts/Random_Image.js
@@ -16,7 +16,16 @@ animalButton.addEventListener('click', Random_Animal);
 function Random_Animal() {
     if (Choice_animals.value in images) {
         fetch(images[Choice_animals.value])
-            .then(response => response.json())
+            .then(response => {
+                if (response.ok) {
+                    return response.json()
+                } else if (response.status === 404) {
+                    Toast_Waring("對方網址掛了...再等一段時間吧!");
+                    return Promise.reject('error 404')
+                } else {
+                    return Promise.reject('some other error: ' + response.status)
+                }
+            })
             .then(data => {
                 animalImage.innerHTML = `<img src="${data.message || data.image || data[0].url}"/>`;
             })
@@ -26,9 +35,28 @@ function Random_Animal() {
             return obj[keys[keys.length * Math.random() << 0]];
         };
         fetch(Array_random(images))
-            .then(response => response.json())
+            .then(response => {
+                if (response.ok) {
+                    return response.json()
+                } else if (response.status === 404) {
+                    Toast_Waring("對方網址掛了...再等一段時間吧!");
+                    return Promise.reject('error 404')
+                } else {
+                    return Promise.reject('some other error: ' + response.status)
+                }
+            })
             .then(data => {
                 animalImage.innerHTML = `<img src="${data.message || data.image || data[0].url}"/>`;
             })
     }
+}
+
+//彈出提示框(警告詢問)
+var toastLiveExample_Waring = document.getElementById('liveToast-Waringg'); // 提示框
+var host_name_Waring = document.getElementById('host_name_Waringg'); //寫入訊息
+
+function Toast_Waring(message) {
+    host_name_Waring.innerHTML = message;
+    let toast = new bootstrap.Toast(toastLiveExample_Waring);
+    toast.show();
 }
